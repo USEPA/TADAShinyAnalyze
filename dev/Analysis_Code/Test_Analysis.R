b48e43c076d187877081b1037a7065b5d09be59f
sheilasaia <sheila.saia@tetratech.com>
2025-07-12 17:11:24 -0400

    added missing package namespaces

--- dev/Analysis_Code/Test_Analysis.R
+++ dev/Analysis_Code/Test_Analysis.R
@@ -12,29 +12,29 @@ library(tigris)
 ### Load criteria tables related files
 
 ## Load the criteria table
-criteria_table <- read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx", 
+criteria_table <- readxl::read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx", 
                              sheet = "TADA-Format Criteria")
 
 # Convert the fraction to upper case
 criteria_table <- criteria_table |>
-  dplyr::mutate(Fraction = toupper(Fraction)) %>%
-  drop_na(TADA.CharacteristicName)
+  dplyr::mutate(Fraction = toupper(Fraction)) |>
+  tidyr::drop_na(TADA.CharacteristicName)
 
 ## Load equation tables
 # Equation based on hardness
-hardness_equation <- read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
+hardness_equation <- readxl::read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
                                sheet = "Hardness_eq")
 
 # Equation based on pH and hardness
-pH_Hardness_equation <- read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
+pH_Hardness_equation <- readxl::read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
                                   sheet = "pH_Hardness_eq")
 
 # Equation based on pH
-pH_equation <- read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
+pH_equation <- readxl::read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
                          sheet = "pH_eq")
 
 # Equation based on pH and Temperature
-pH_Temperature_equation <- read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
+pH_Temperature_equation <- readxl::read_excel("Data/TADA_Format_Criteria_Table_DRAFT_20250710.xlsx",
                                      sheet = "pH_Temperature_eq")
 
 ### Load the example dataset
@@ -45,13 +45,14 @@ pH_Temperature_equation <- read_excel("Data/TADA_Format_Criteria_Table_DRAFT_202
 # These three files are assumed to be uploaded by users from the Load tab
 
 # The input file
-dat <- read_csv("Data/Example_JoinAU/tada_jointoau_output_ts20250709033312_copy_input_file.csv")
+dat <- readr::read_csv("Data/Example_JoinAU/tada_jointoau_output_ts20250709033312_copy_input_file.csv")
 
 # The AU to Use
-AU_Use <- read_csv("Data/Example_JoinAU/tada_jointoau_output_ts20250709033312_autouse_for_review.csv")
+AU_Use <- readr::read_csv("Data/Example_JoinAU/tada_jointoau_output_ts20250709033312_autouse_for_review.csv")
 
 # The AU to MLID
-AU_MLID <- read_csv("Data/Example_JoinAU/tada_jointoau_output_ts20250709033312_mltoaus_for_review.csv")
+AU_MLID <- readr::read_csv("Data/Example_JoinAU/tada_jointoau_output_ts20250709033312_mltoaus_for_review.csv") |>
+  dplyr::filter(Needs_Review == "No")
 
 # Convert ActivityStartDateTime to dateTime
 dat <- dat |>
@@ -116,10 +117,10 @@ dat2 <- dat |>
 
 # Join the criteria_table_f1 and AU_MLID_f1 to dat2
 dat3 <- dat2 |>
-  left_join(AU_MLID_f1) |>
-  left_join(AU_Use_f1, 
-            by = "JoinToAU.AssessmentUnitIdentifier",
-            relationship = "many-to-many") |>
+  dplyr::left_join(AU_MLID_f1) |>
+  dplyr::left_join(AU_Use_f1, 
+                   by = "JoinToAU.AssessmentUnitIdentifier",
+                   relationship = "many-to-many") |>
   criteria_join(criteria_table_f1) 
 
 ### Step 3: Separate the dataset based on if criteria exist
