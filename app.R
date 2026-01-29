785bed1da79c4d098bf7a4faf58a8d8272f15e23
Mullin <Mullin.Cristina@epa.gov>
2026-01-29 09:58:29 -0500

    Prepare for deployment

--- app.R
+++ app.R
@@ -1,7 +1,13 @@
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
+# Launch the ShinyApp (Do not remove this file)
+# To deploy, run the script in 04_deploy.R
+
+# Suppress Shiny support autoloading (extra safety; not strictly necessary if inst/app has no R/)
+options(shiny.autoload.r = FALSE)
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
-TADAShinyAnalyze::run_app() # add parameters here (if any)
+
+# Golem production mode
+options(golem.app.prod = TRUE)
+
+# Start the golem app
+run_app()
