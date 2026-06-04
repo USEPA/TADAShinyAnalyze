082b8695ff3cd3b80ed12af49399d2b187e821bc
Mullin <Mullin.Cristina@epa.gov>
2026-06-04 15:34:09 -0400

    update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -2,23 +2,21 @@
 #'
 #' @param ... arguments to pass to golem_opts.
 #' See `?golem::get_golem_options` for more details.
-#' @inheritParams shiny::shinyApp
 #'
 #' @export
-#' @importFrom shiny shinyApp
-#' @importFrom golem with_golem_options
+#'
 run_app <- function(
-  onStart = NULL,
-  options = list(),
-  enableBookmarking = NULL,
-  uiPattern = "/",
-  ...
+    onStart = NULL,
+    options = list(),
+    enableBookmarking = NULL,
+    uiPattern = "/",
+    ...
 ) {
   # set options (moved from top-level to avoid running during package load)
   options(shiny.maxRequestSize = get_golem_config("MB_LIMIT") * 1024^2)
-
-  with_golem_options(
-    app = shinyApp(
+  
+  golem::with_golem_options(
+    app = shiny::shinyApp(
       ui = app_ui,
       server = app_server,
       onStart = onStart,
